04f5905fe329dcb09a6bd92f0162220602cc8083
Adarsh-thedev <adarshpittu@gmail.com>
2020-06-23 13:10:46 +0530

    new Booking component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Logo from './Components/Logo';
 import './App.css';
 import Header from './Components/Header';
+import Booking from './Components/Booking';
 
 function App() {
   return (
     <React.Fragment>
       <Logo/>
       <Header/>
+      <Booking/>
     </React.Fragment>
   );
 }
