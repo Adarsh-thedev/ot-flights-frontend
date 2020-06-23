e3f0bc9be5d11e58603facf4ea1d8d0ca45df952
Adarsh-thedev <adarshpittu@gmail.com>
2020-06-23 13:09:50 +0530

    changes in header style and font size

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,15 +4,16 @@ import Background from './home-img.png';
 const sectionStyle = {
     backgroundImage : `url(${Background})`,
     minHeight : 200,
-    width : '100%',
     backgroundRepeat : 'no-repeat'
 }
 
 const Header = () => {
     return(
-        <div style = {sectionStyle}>
-            <div className = 'col-md-4' style = {{float : 'right'}}>
-                <h1 className = 'text-white f1 right'>FLY WITH RESPONSIBLY WITH OPENTABS</h1>
+        <div>
+            <div style = {sectionStyle}>
+                <div className = 'col-md-4' style = {{float : 'right'}}>
+                    <p className = 'text-white h1 mt5'>FLY WITH RESPONSIBLY WITH OPENTABS</p>
+                </div>
             </div>
         </div>
     );
