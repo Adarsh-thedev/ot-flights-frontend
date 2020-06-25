9cdaed9af8c47068c3c775e460663536814953fe
Adarsh-thedev <adarshpittu@gmail.com>
2020-06-25 15:39:19 +0530

    Updates

---
+++ precache-manifest.9ed589aa61caf901e536bf57f5576cfb.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbca7758d061bf9def87d011e14184d5",
+    "url": "/ot-flights-frontend/index.html"
+  },
+  {
+    "revision": "493d58573a99daf8fd40",
+    "url": "/ot-flights-frontend/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "cd6de28ccb3d27a5befb",
+    "url": "/ot-flights-frontend/static/css/main.c49d760f.chunk.css"
+  },
+  {
+    "revision": "493d58573a99daf8fd40",
+    "url": "/ot-flights-frontend/static/js/2.f5820457.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ot-flights-frontend/static/js/2.f5820457.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd6de28ccb3d27a5befb",
+    "url": "/ot-flights-frontend/static/js/main.749e6b78.chunk.js"
+  },
+  {
+    "revision": "a3c0bf33e5ecbf3d816d",
+    "url": "/ot-flights-frontend/static/js/runtime-main.142c272f.js"
+  },
+  {
+    "revision": "c3f5250c4cddf4cbb1dd741d02f1f1cf",
+    "url": "/ot-flights-frontend/static/media/about-carbon.c3f5250c.jpg"
+  },
+  {
+    "revision": "05ad5d34fef9702daf37f6f9f320284a",
+    "url": "/ot-flights-frontend/static/media/home-img.05ad5d34.png"
+  },
+  {
+    "revision": "e3f47e63e869d8d255e7a940a1781efa",
+    "url": "/ot-flights-frontend/static/media/jungle.e3f47e63.jpg"
+  },
+  {
+    "revision": "83af5f6801f6b5c764ee5cd446d9b804",
+    "url": "/ot-flights-frontend/static/media/rainforest.83af5f68.jpg"
+  }
+]);
