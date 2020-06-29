0201339cc655cd343aa701654b2d3a8aef979e96
Adarsh-thedev <adarshpittu@gmail.com>
2020-06-29 09:45:13 +0530

    fixed routing issue

--- src/Components/Logo.js
+++ src/Components/Logo.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import OpenTabsLogo from '../assets/logo.png';
+import { Link } from 'react-router-dom';
 
 const Logo = () => {
     return(
         <div className = 'nav' id = '#tabs'>
             <div className = 'container'>
-                <a className = 'navbar-brand' href = '/'>
+                <Link className = 'navbar-brand' to = '/'>
                     <img className = 'mr3 dim' src = {OpenTabsLogo} alt = 'logo'/>
                     <span style = {{fontFamily : 'Nunito, sans-serif', verticalAlign : 'middle'}} className = 'dib dim h2 b text-primary'>OpenTabs</span>
-                </a>
+                </Link>
             </div>
         </div>
     );
