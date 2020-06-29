e7cccb14c668a6e7291b07b7586e9528996d2134
Adarsh-thedev <adarshpittu@gmail.com>
2020-06-29 18:57:42 +0530

    source dest, change in styles

--- src/Components/FlightSearchResults.js
+++ src/Components/FlightSearchResults.js
@@ -3,18 +3,20 @@ import { Link } from 'react-router-dom';
 import Base from './Base';
 
 const FlightSearchResults = (props) => {
-    const {searchResult} = props.location.state;
+    const {searchResult, source, destination} = props.location.state;
     return(
         <Base>
-            <div className = 'container center'>
-                <Link to = '/' >
-                    <button className = 'btn blue text-white br-pill dim b mt4'>
-                        <i className="fa fa-home mr-2" aria-hidden="true"></i>
-                        Back to flight search
-                    </button>
+            <div className = 'container center mt2'>
+                <h5>Flights from {source.slice(0, source.length - 5)} to {destination.slice(0, destination.length - 5)}</h5>
+                <Link to = '/' className = 'badge f4 pa2 shadow-3 blue bn text-white br-pill dim b ma2'>
+                    <i className="fa fa-home mr-2" aria-hidden="true"></i>
+                    Back to flight search
                 </Link>
-                <h1>Flight Search Results will be displayed here</h1>
-                <p>{JSON.stringify(searchResult[0])}</p>
+                <div className = 'row'>
+                    <div className = 'card col-12 br3 pa2'>
+                        <p>{JSON.stringify(searchResult[0])}</p>
+                    </div>
+                </div>
             </div>
         </Base>
     );
