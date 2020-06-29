a3a6bda0a95704dd81b0b48502d0a85cd6fbd8f3
Adarsh-thedev <adarshpittu@gmail.com>
2020-06-29 09:35:48 +0530

    added button to easy return at home page

--- src/Components/FlightSearchResults.js
+++ src/Components/FlightSearchResults.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Base from './Base';
 
 const FlightSearchResults = (props) => {
     const {searchResult} = props.location.state;
     return(
-        <div className = 'container center'>
-            <Link to = '/' ><h1>Back to home page</h1></Link>
-            <h1>Flight Search Results will be displayed here</h1>
-            <p>{JSON.stringify(searchResult[0])}</p>
-        </div>
+        <Base>
+            <div className = 'container center'>
+                <Link to = '/' >
+                    <button className = 'btn blue text-white br-pill dim b mt4'>
+                        <i className="fa fa-home mr-2" aria-hidden="true"></i>
+                        Back to flight search
+                    </button>
+                </Link>
+                <h1>Flight Search Results will be displayed here</h1>
+                <p>{JSON.stringify(searchResult[0])}</p>
+            </div>
+        </Base>
     );
 }
 
