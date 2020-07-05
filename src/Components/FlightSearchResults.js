2ad3e9af6bf2b3e75123719093488f18e0b622f2
Adarsh-thedev <adarshpittu@gmail.com>
2020-07-05 14:07:40 +0530

    requirements changed, use no routes

--- src/Components/FlightSearchResults.js
+++ src/Components/FlightSearchResults.js
@@ -1,24 +1,19 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import Base from './Base';
+import Flight from './Flight';
 
-const FlightSearchResults = (props) => {
-    const {searchResult, source, destination} = props.location.state;
+const FlightSearchResults = ({searchResult}) => {
     return(
-        <Base>
-            <div className = 'container center mt2'>
-                <h5>Flights from {source.slice(0, source.length - 5)} to {destination.slice(0, destination.length - 5)}</h5>
-                <Link to = '/' className = 'badge f4 pa2 shadow-3 blue bn text-white br-pill dim b ma2'>
-                    <i className="fa fa-home mr-2" aria-hidden="true"></i>
-                    Back to flight search
-                </Link>
-                <div className = 'row'>
-                    <div className = 'card col-12 br3 pa2'>
-                        <p>{JSON.stringify(searchResult[0])}</p>
-                    </div>
-                </div>
+        <div className = 'center container'>
+            <h5>Search Results</h5>
+            <div>
+                {Object.values(searchResult[0]).flatMap(airport =>
+                    Object.values(airport).map(({flight_number, price})=> (
+                        // console.table(flight)
+                        <Flight flightNumber={flight_number} price={price} />
+                    ))
+                )}
             </div>
-        </Base>
+        </div>
     );
 }
 
