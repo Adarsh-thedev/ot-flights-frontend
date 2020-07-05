a3fb90ab1d4d8ff79d9012dc59a4b2604b51495c
Adarsh-thedev <adarshpittu@gmail.com>
2020-07-05 14:08:59 +0530

    cle

--- src/Components/Logo.js
+++ src/Components/Logo.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import OpenTabsLogo from '../assets/logo.png';
-import { Link } from 'react-router-dom';
 
 const Logo = () => {
     return(
         <div className = 'nav' id = '#tabs'>
             <div className = 'container'>
-                <Link className = 'navbar-brand' to = '/'>
-                    <img className = 'mr3 dim' src = {OpenTabsLogo} alt = 'logo'/>
-                    <span style = {{fontFamily : 'Nunito, sans-serif', verticalAlign : 'middle'}} className = 'dib dim h2 b text-primary'>OpenTabs</span>
-                </Link>
+                <img className = 'mr3 dim' src = {OpenTabsLogo} alt = 'logo'/>
+                <span style = {{fontFamily : 'Nunito, sans-serif', verticalAlign : 'middle'}} className = 'dib dim h2 b text-primary'>OpenTabs</span>
             </div>
         </div>
     );
