2404eeefddcd03b36760314b5689f5333ba76afa
Adarsh-thedev <adarshpittu@gmail.com>
2020-07-05 17:35:14 +0530

    getting result data

--- src/Components/Flight.js
+++ src/Components/Flight.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Flight = ({flightNumber, price}) => {
     return (
-        <div>
-            <p>{flightNumber}</p>
-            <p>{price}</p>
+        <div className = 'bg1 br3 shadow-3 mt2 mb2 pa3'>
+            <span>Flight Number : {flightNumber}</span>
+            <span className = 'ml2'>Price : {price} €</span>
         </div>
     );
 }
