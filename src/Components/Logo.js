8a6c7b6ebf6764490c2f5cab876d1d63b34c64e6
Adarsh-thedev <adarshpittu@gmail.com>
2020-07-11 17:21:24 +0530

    more accurate

--- src/Components/Logo.js
+++ src/Components/Logo.js
@@ -5,8 +5,8 @@ const Logo = () => {
     return(
         <div className = 'nav nunito' id = '#tabs'>
             <div className = 'container pointer'>
-                <img className = 'mr3 dim' src = {OpenTabsLogo} alt = 'logo'/>
-                <span style = {{position : 'relative',top : '10px'}} className = 'ml2 dim f2 text-primary'>OpenTabs</span>
+                <img className = 'mr3 dim pt1' src = {OpenTabsLogo} alt = 'logo'/>
+                <span style = {{verticalAlign : 'middle'}} className = 'ml2 dim f2 text-primary'>OpenTabs</span>
             </div>
         </div>
     );
