1120d87b0df0657a8bec71b43fb939b0f1cf4efb
Adarsh-thedev <adarshpittu@gmail.com>
2020-07-11 12:03:43 +0530

    minor fixes

--- src/Components/Logo.js
+++ src/Components/Logo.js
@@ -3,10 +3,10 @@ import OpenTabsLogo from '../assets/logo.png';
 
 const Logo = () => {
     return(
-        <div className = 'nav' id = '#tabs'>
-            <div className = 'container'>
+        <div className = 'nav nunito' id = '#tabs'>
+            <div className = 'container pointer'>
                 <img className = 'mr3 dim' src = {OpenTabsLogo} alt = 'logo'/>
-                <span style = {{fontFamily : 'Nunito, sans-serif', verticalAlign : 'middle'}} className = 'dib dim h2 b text-primary'>OpenTabs</span>
+                <span style = {{position : 'relative',top : '10px'}} className = 'ml2 dim f2 text-primary'>OpenTabs</span>
             </div>
         </div>
     );
