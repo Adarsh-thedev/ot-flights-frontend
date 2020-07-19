836c8678c33877085f7023e6df939c7735509752
Adarsh-thedev <adarshpittu@gmail.com>
2020-07-19 17:49:02 +0530

    Carbon offset payments page

---
+++ src/Components/CarbonOffset.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Base from './Base';
+
+const CarbonOffset = () => {
+   return(
+    <Base>
+        <h1>CarbonOffset Details</h1>
+    </Base>
+   );
+}
+
+export default CarbonOffset;
