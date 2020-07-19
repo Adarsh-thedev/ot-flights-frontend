60d18d540afa117b9b87ef9ce1d5817499cd823c
Adarsh-thedev <adarshpittu@gmail.com>
2020-07-19 17:47:38 +0530

    introduced routes

--- src/App.js
+++ src/App.js
@@ -1,28 +1,14 @@
 import React from 'react';
-import Logo from './Components/Logo';
-import Header from './Components/Header';
-import Slide from 'react-reveal/Slide';
-import Stats from './Components/Stats';
-import AboutOpenTabs from './Components/AboutOpenTabs';
-import AboutCarbonOffset from './Components/AboutCarbonOffset';
-import AboutRainForest from './Components/AboutRainForest';
-import Footer from './Components/Footer';
 import './App.css';
+import Base from './Components/Base';
 import FlightSearch from './Components/Flightsearch';
 
 function App() {
   return (
     <React.Fragment>
-      <Logo/>
-            <Header/>
-            <FlightSearch/>
-            <Slide right>
-                <Stats/>
-                <AboutOpenTabs/>
-                <AboutCarbonOffset/>
-                <AboutRainForest/>
-            </Slide>
-            <Footer/>
+      <Base>
+        <FlightSearch/>
+      </Base>
     </React.Fragment>
   );
 } 
