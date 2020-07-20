5a5eb9389eb6cf17079dc79e7170f7deb4b622d6
Adarsh-thedev <adarshpittu@gmail.com>
2020-07-20 14:13:29 +0530

    style and route fixing

--- src/Components/Logo.js
+++ src/Components/Logo.js
@@ -4,10 +4,10 @@ import OpenTabsLogo from '../assets/logo.png';
 const Logo = () => {
     return(
         <div className = 'nav nunito' id = '#tabs'>
-            <div className = 'container pointer'>
+            <a href = '/' className = 'container pointer'>
                 <img className = 'mr3 dim pt1' src = {OpenTabsLogo} alt = 'logo'/>
                 <span style = {{verticalAlign : 'middle'}} className = 'ml2 dim f2 text-primary'>OpenTabs</span>
-            </div>
+            </a>
         </div>
     );
 }
