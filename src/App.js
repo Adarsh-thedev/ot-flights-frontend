aa81e4ac864dac56bd9f13a22dab80b1b02e7374
Adarsh-thedev <adarshpittu@gmail.com>
2020-07-21 18:10:16 +0530

    changing views

--- src/App.js
+++ src/App.js
@@ -2,12 +2,23 @@ import React from 'react';
 import './App.css';
 import Base from './Components/Base';
 import FlightSearch from './Components/Flightsearch';
+import Slide from 'react-reveal/Slide';
+import Stats from './Components/Stats';
+import AboutOpenTabs from './Components/AboutOpenTabs';
+import AboutCarbonOffset from './Components/AboutCarbonOffset';
+import AboutRainForest from './Components/AboutRainForest';
 
 function App() {
   return (
     <React.Fragment>
       <Base>
         <FlightSearch/>
+        <Slide right>
+            <Stats/>
+            <AboutOpenTabs/>
+            <AboutCarbonOffset/>
+            <AboutRainForest/>
+        </Slide>
       </Base>
     </React.Fragment>
   );
