c4938ca0be91046d6f42be6e0412781c5f5dc465
Adarsh-thedev <adarshpittu@gmail.com>
2020-07-25 08:32:55 +0530

    how it works component

--- src/App.js
+++ src/App.js
@@ -1,27 +1,29 @@
-import React from 'react';
-import './App.css';
-import Base from './Components/Base';
-import FlightSearch from './Components/Flightsearch';
-import Slide from 'react-reveal/Slide';
-import Stats from './Components/Stats';
-import AboutOpenTabs from './Components/AboutOpenTabs';
-import AboutCarbonOffset from './Components/AboutCarbonOffset';
-import AboutRainForest from './Components/AboutRainForest';
+import React from "react";
+import "./App.css";
+import Base from "./Components/Base";
+import FlightSearch from "./Components/Flightsearch";
+import Slide from "react-reveal/Slide";
+import Stats from "./Components/Stats";
+import AboutOpenTabs from "./Components/AboutOpenTabs";
+import AboutCarbonOffset from "./Components/AboutCarbonOffset";
+import AboutRainForest from "./Components/AboutRainForest";
+import HowItWorks from "./Components/HowItWorks";
 
-function App() {
+const App = () => {
   return (
     <React.Fragment>
       <Base>
-        <FlightSearch/>
+        <FlightSearch />
         <Slide right>
-            <Stats/>
-            <AboutOpenTabs/>
-            <AboutCarbonOffset/>
-            <AboutRainForest/>
+          <Stats />
+          <AboutOpenTabs />
+          <HowItWorks />
+          <AboutCarbonOffset />
+          <AboutRainForest />
         </Slide>
       </Base>
     </React.Fragment>
   );
-} 
+};
 
 export default App;
