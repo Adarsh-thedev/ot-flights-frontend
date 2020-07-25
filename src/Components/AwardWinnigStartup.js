e5df6386d5b631b9a07e89a484c1c0fb1c781691
Adarsh-thedev <adarshpittu@gmail.com>
2020-07-25 10:49:53 +0530

    award winning startup

--- src/Components/AwardWinnigStartup.js
+++ src/Components/AwardWinnigStartup.js
@@ -25,6 +25,82 @@ const AwardWinnigStartup = () => {
               <p>Social Tech Accelerator</p>
             </div>
           </div>
+          <div className="col-xs-12 col-sm-4 col-md-2 mb2">
+            <a
+              href="https://hatchcolab.ch"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {" "}
+              <img
+                className="grow d-flex center"
+                src="https://raw.githubusercontent.com/greco-paro-association/shared-hosting/master/images/Jacobs.png"
+                width="160px"
+                alt="card2"
+              />
+            </a>
+            <div className="col f4" style={{ color: "#444" }}>
+              <p className="mb-0 b pb-2 pt4">Finalist</p>
+              <p>Jacobs Startup Competition</p>
+            </div>
+          </div>
+          <div className="col-xs-12 col-sm-4 col-md-2 mb2">
+            <a
+              href="https://hatchcolab.ch"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {" "}
+              <img
+                className="grow d-flex center"
+                src="https://raw.githubusercontent.com/greco-paro-association/shared-hosting/master/images/IE.png"
+                width="160px"
+                alt="card3"
+              />
+            </a>
+            <div className="col f4" style={{ color: "#444" }}>
+              <p className="mb-0 b pb-2 pt4">Winner</p>
+              <p>Sustainable Entrepreneurship</p>
+            </div>
+          </div>
+          <div className="col-xs-12 col-sm-4 col-md-2 mb2">
+            <a
+              href="https://hatchcolab.ch"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {" "}
+              <img
+                className="grow d-flex center"
+                src="https://raw.githubusercontent.com/greco-paro-association/shared-hosting/master/images/santander.png"
+                width="160px"
+                alt="card4"
+              />
+            </a>
+            <div className="col f4" style={{ color: "#444" }}>
+              <p className="mb-0 b pb-2 pt4">Participant</p>
+              <p>Young People With Solutions</p>
+            </div>
+          </div>
+          <div className="col-xs-12 col-sm-4 col-md-2 mb2">
+            <a
+              href="https://hatchcolab.ch"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {" "}
+              <img
+                className="grow d-flex center"
+                src="https://raw.githubusercontent.com/greco-paro-association/shared-hosting/master/images/google-adgrant.png"
+                width="160px"
+                alt="card5"
+              />
+            </a>
+            <div className="col f4" style={{ color: "#444" }}>
+              <p className="mb-0 b pb-2 pt4">Recipient</p>
+              <p>Google AdGrant</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
